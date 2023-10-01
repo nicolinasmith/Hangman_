f3b9d1080f326156ccf92d7029c6c080ba640bc6
Nicolina Smith <113508253+nism07@users.noreply.github.com>
2023-10-01 17:38:11 +0200

    Bokstäver och commands

--- Hangman_/ViewModels/GameViewModel.cs
+++ Hangman_/ViewModels/GameViewModel.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Hangman_.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Hangman_.ViewModels
 {
@@ -10,18 +13,31 @@ namespace Hangman_.ViewModels
     {
         public string RandomWord { get; set; }
 
+        public ICommand GuessLetterCommand { get; set; }
+
         public List<char> Alphabet { get; set; }
 
         public GameViewModel(string randomWord)
         {
             RandomWord = randomWord;
 
+            CreateListOfAlphabet();
+
+            GuessLetterCommand = new RelayCommand(letter => GuessLetter((char)letter));
+        }
+
+        private void GuessLetter(char letter)
+        {
+            MessageBox.Show("HEJ");
+        }
+
+        public void CreateListOfAlphabet()
+        {
             Alphabet = new List<char>();
             for (char letter = 'A'; letter <= 'Z'; letter++)
             {
                 Alphabet.Add(letter);
             }
-            //DataContext = this;
         }
     }
 }
