e584438995996e89d8e88cc9e0530c8bf0ac9310
Nicolina Smith <113508253+nism07@users.noreply.github.com>
2023-10-01 22:38:53 +0200

    Converter och enum, synlighet vid gissningar

---
+++ Hangman_/Models/WordChar.cs
@@ -0,0 +1,25 @@
+﻿using Hangman_.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Hangman_.Models
+{
+    public class WordChar : DependencyObject
+    {
+        public char LetterOfWord { get; set; }
+
+        public LetterVisibilityStatus CharVisibility
+        {
+            get { return (LetterVisibilityStatus)GetValue(CharVisibilityProperty); }
+            set { SetValue(CharVisibilityProperty, value); }
+        }
+
+        public static readonly DependencyProperty CharVisibilityProperty =
+            DependencyProperty.Register("CharVisibility", typeof(LetterVisibilityStatus), typeof(WordChar), new PropertyMetadata(LetterVisibilityStatus.GuessWrong));
+
+    }
+}
